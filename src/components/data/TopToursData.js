cf28fa4e91e7cf359e4cfafbf43ec6446fbb8a40
Kyle Lynch <kylelynch104@gmail.com>
2020-02-22 17:24:43 -0600

    move image folder to src

--- src/components/data/TopToursData.js
+++ src/components/data/TopToursData.js
@@ -1,52 +1,63 @@
+import img1 from '../../images/topTours/skyline.jpg';
+import img2 from '../../images/topTours/bluebonnets.jpg';
+import img3 from '../../images/topTours/bikes.jpg';
+import img4 from '../../images/topTours/capitol.jpg';
+import img5 from '../../images/topTours/hamilton.jpg';
+import img6 from '../../images/topTours/cows.jpg';
+import img7 from '../../images/topTours/art.jpg';
+import img8 from '../../images/topTours/ship.jpg';
+import img9 from '../../images/sanAntonio.jpg';
+import img10 from '../../images/topTours/cafe.jpg';
+
 export const TopTourData = [
   {
     id: 100,
     title: 'Austin Downtown',
-    img: './images/topTours/skyline.jpg'
+    img: img1
   },
   {
     id: 101,
     title: 'Bluebonnets',
-    img: './images/topTours/bluebonnets.jpg'
+    img: img2
   },
   {
     id: 102,
     title: 'Bicycle Austin',
-    img: './images/topTours/bikes.jpg'
+    img: img3
   },
   {
     id: 103,
     title: 'Capitol',
-    img: './images/topTours/capitol.jpg'
+    img: img4
   },
   {
     id: 104,
     title: "Hamilton's Pool",
-    img: './images/topTours/hamilton.jpg'
+    img: img5
   },
   {
     id: 105,
     title: 'Ranches',
-    img: './images/topTours/cows.jpg'
+    img: img6
   },
   {
     id: 106,
     title: 'Street Art',
-    img: './images/topTours/art.jpg'
+    img: img7
   },
   {
     id: 107,
     title: 'Battleship Texas',
-    img: './images/topTours/ship.jpg'
+    img: img8
   },
   {
     id: 108,
     title: 'San Antonio',
-    img: './images/sanAntonio.jpg'
+    img: img9
   },
   {
     id: 109,
     title: 'Hip Cafes',
-    img: './images/topTours/cafe.jpg'
+    img: img10
   }
 ];
--- src/components/data/TourData.js
+++ src/components/data/TourData.js
@@ -1,24 +1,29 @@
+import tourImg1 from '../../images/topTours/skyline.jpg';
+import tourImg2 from '../../images/topTours/hamilton.jpg';
+import tourImg3 from '../../images/topTours/capitol.jpg';
+import tourImg4 from '../../images/sanAntonio.jpg';
+
 export const TourData = [
   {
     id: 1,
     city: 'Downtown Austin',
-    img: './images/topTours/skyline.jpg',
+    img: tourImg1,
     tour: 'Walk Downtown Austin',
     info:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptas ipsa delectus quos, eius at.'
   },
   {
     id: 2,
-    city: 'Hamilton\'s Pool',
-    img: './images/topTours/hamilton.jpg',
-    tour: 'Day Trip to Hamilton\'s Pool',
+    city: "Hamilton's Pool",
+    img: tourImg2,
+    tour: "Day Trip to Hamilton's Pool",
     info:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptas ipsa delectus quos, eius at.'
   },
   {
     id: 3,
     city: 'Austin Capitol',
-    img: './images/topTours/capitol.jpg',
+    img: tourImg3,
     tour: 'Capitol Building Tour',
     info:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptas ipsa delectus quos, eius at.'
@@ -26,7 +31,7 @@ export const TourData = [
   {
     id: 4,
     city: 'san antonio',
-    img: './images/sanAntonio.jpg',
+    img: tourImg4,
     tour: 'San Antonio Historic Missions Tour',
     info:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptas ipsa delectus quos, eius at.'
