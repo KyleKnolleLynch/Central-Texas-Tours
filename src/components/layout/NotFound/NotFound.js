34c1208ff3032280a2c3ca32ebee9d75f677c699
Kyle Lynch <kylelynch104@gmail.com>
2020-02-22 18:49:36 -0600

    replace layout

---
+++ src/components/layout/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <h1 className='text-danger'>Error 404</h1>
+      <h1>Page Not Found!</h1>
+      <p className='lead'>The page you requested does not exist.</p>
+      <Link to='/'>Return to Home Page here.</Link>{' '}
+      <i className='fas fa-arrow-left'></i>
+    </Fragment>
+  );
+};
+
+export default NotFound;
---
+++ src/components/layout/TopTours/TopTours.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TopTourData } from '../../data/TopToursData';
+
+import './TopTours.scss';
+
+const TopTours = () => {
+  return (
+    <div className='top-tours-wrapper'>
+      <h2>
+        <span className='text-primary'>Popular</span>
+        <span className='light'> Tours</span>
+      </h2>
+      <div className='top-tours'>
+        {TopTourData.map(tour => (
+          <div key={tour.id}>
+            <img src={tour.img} alt='topTour' />
+            <h2>{tour.title}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopTours;
